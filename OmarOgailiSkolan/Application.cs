2e1da36df84373c1f79de00e8afd5a55eee09379
omar ogaili <ogailiomar@hotmail.com>
2024-03-31 09:42:39 +0200

    projektet startar jag har lagt till databasen och andra små functioner i Create, och index

---
+++ OmarOgailiSkolan/Application.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OmarOgailiSkolan
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required, DataType(DataType.Date)]
+        public DateTime Dateofbirth { get; set; }
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ OmarOgailiSkolan/Data/OmarOgailiSkolanContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OmarOgailiSkolan;
+
+namespace OmarOgailiSkolan.Data
+{
+    public class OmarOgailiSkolanContext : DbContext
+    {
+        public OmarOgailiSkolanContext (DbContextOptions<OmarOgailiSkolanContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OmarOgailiSkolan.Application> Application { get; set; } = default!;
+    }
+}
